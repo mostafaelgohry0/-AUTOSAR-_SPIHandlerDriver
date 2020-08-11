0f9bb3ac61194700d2ee35d514f613cab6807591
mostafa <elgohryeng@gmail.com>
2020-08-12 01:41:57 +0200

    update Det

--- Det.c
+++ Det.c
@@ -10,3 +10,91 @@
  *     Created on: Aug 7, 2020
  */
 
+#include "Det.h"
+
+/*********************************Function definitions**********************/
+
+/********************************************************************
+*Service name: Det_Init 
+*Service ID[hex]: 0x00 
+*Sync/Async: Synchronous
+*Reentrancy:  Non Reentrant
+*Parameters (in): None 
+*Parameters (inout): None 
+*Parameters (out): None 
+*Return value: None 
+*Description: Service to initialize the Development Error Tracer. 
+*********************************************************************/
+
+void  Det_Init(void){
+
+}
+
+
+
+/********************************************************************
+*Service name: Det_ReportError
+*Service ID[hex]: 0x01 
+*Sync/Async: Depending  on  implemented  functionality
+*Reentrancy:  Reentrant
+*Parameters (in): ModuleId   --->  Module ID of calling module.
+*                 InstanceId --->  The identifier of the index based instance of a module,
+*                                  starting  from 0, If the module is a single instance module it shall 
+*                                  pass 0 as the InstanceId.
+*                 ApiId --->ID of API service in which error is detected(defined in SWS of calling module)
+*                 ErrorId --->     ID  of detected development error(defined in SWS of calling module).  
+*Parameters (inout): None 
+*Parameters (out): None 
+*Return value: None 
+*Description: Service to report development errors. 
+*********************************************************************/
+
+Std_ReturnType Det_ReportError(uint16 ModuleId,
+							   uint8 InstanceId,
+							   uint8 ApiId,
+							   uint8 ErrorId)
+{
+
+
+
+}
+
+
+
+
+/********************************************************************
+*Service name: Det_Start
+*Service ID[hex]: 0x02 
+*Sync/Async: Synchronous 
+*Reentrancy:  Non Reentrant
+*Parameters (in): None 
+*Parameters (inout): None 
+*Parameters (out): None 
+*Return value: None 
+*Description: Service to start the Development Error Tracer.
+*********************************************************************/
+
+void Det_Start(void) {
+
+}
+
+
+
+/********************************************************************
+*Service name: Det_GetVersionInfo
+*Service ID[hex]: 0x02 
+*Sync/Async: Synchronous 
+*Reentrancy: Reentrant
+*Parameters (in): None 
+*Parameters (inout): None 
+*Parameters (out): versioninfo Pointer to where to store the version information of this module. 
+*Return value: None 
+*Description:Returns the version information of this module. 
+*********************************************************************/
+
+void Det_GetVersionInfo(Std_VersionInfoType* versioninfo) {
+
+	
+}
+
+
--- Det.h
+++ Det.h
@@ -1,9 +1,9 @@
 /*
- *     Module :
+ *     Module : DET 
  *
- *     File Name  :
+ *     File Name  :Det.h 
  *
- *     Description :
+ *     Description :header file of Development Error Tracer Det
  *
  *     Author: Mostafa Elgohary
  *
@@ -22,6 +22,46 @@
 #ifndef DET_H_
 #define DET_H_
 
+/*  this id for company in autosar  I put it  0  for  test only  */ 
+#define DET_VENDOR_ID     (0U) 
 
+#define DET_MODULE_ID     (015U)
+
+#define DET_INSTANCE_ID   (0U)
+
+
+
+
+/*
+ *  Module Version 1.0.0
+ */
+#define DET_SW_MAJOR_VERSION               (1U)
+#define DET_SW_MINOR_VERSION               (0U)
+#define DET_SW_PATCH_VERSION               (0U)
+
+/*
+ *  AUTOSAR Version 4.0.3
+ */
+#define DET_AR_RELEASE_MAJOR_VERSION              (4U)
+#define DET_AR_RELEASE_MINOR_VERSION              (0U)
+#define DET_AR_RELEASE_PATCH_VERSION              (3U)
+
+
+// checking the  std_types.h  AUTOSAR Version  
+
+#if ((DET_SW_MAJOR_VERSION != STD_AR_RELEASE_MAJOR_VERSION )||\
+     (DET_SW_MINOR_VERSION != STD_AR_RELEASE_MINOR__VERSION )||\
+     (DET_SW_PATCH_VERSION != STD_AR_RELEASE_PATCH_VERSION ))
+#error "std_types.h  have different AUTOSAR Version "
+#endif 
+/*********************************Function definitions**********************/
+
+void  Det_Init(void);
+Std_ReturnType Det_ReportError(uint16 ModuleId,
+							   uint8 InstanceId,
+							   uint8 ApiId,
+							   uint8 ErrorId);
+void Det_Start(void) ;
+void Det_GetVersionInfo(Std_VersionInfoType* versioninfo);
 
 #endif /* DET_H_ */
